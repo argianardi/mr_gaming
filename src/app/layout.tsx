eb4ffecd4000821c8e3b85f3bc8710f1fb4220e6
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-16 05:58:59 +0700

    Feat: Integrate Header and Footer components with enhanced metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Mr Gaming',
+  title: {
+    default: 'Mr Gaming',
+    template: 'Mr Gaming | %s',
+  },
   description:
     'Mr Gaming adalah platform terpercaya di Indonesia yang menyediakan layanan top-up game dan afiliasi voucher game termurah. Nikmati pengalaman bermain game tanpa batas dengan harga terbaik dan layanan profesional.',
 };
@@ -28,8 +33,12 @@ export default function RootLayout({
           sizes="16x16"
         />
       </head>
-      <body className="bg-[#f2f5f7]">
-        <main>{children}</main>
+      <body className="bg-[#f2f5f7] min-h-screen flex flex-col h-[800px]">
+        <Header />
+        <main className="mx-4  sm:mx-6  md:mx-8  lg:mx-10  xl:mx-12  flex-1">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
