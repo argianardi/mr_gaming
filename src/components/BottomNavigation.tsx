7073f2c0baa25100e93ef72b1fcc5b29fe338332
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-16 17:39:13 +0700

    Styles: enhance styling and hover effects

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -43,18 +43,30 @@ const BottomNavigation = () => {
   return (
     <div className="sticky lg:hidden bottom-0 py-4">
       <div
-        className={`btm-nav rounded-t-3xl xs:rounded-t-[50px] overflow-hidden h-16 sm:h-20 lg:h-24 mt-6 w-full bg-base-100 bg-opacity-70 bgNav`}
+        className={`btm-nav  rounded-t-3xl xs:rounded-t-[50px] overflow-hidden h-16 sm:h-20 lg:h-24 mt-6 w-full bg-base-100 bg-opacity-70 bgNav`}
       >
         {navigations.map((nav, index) => (
           <Link
             key={index}
             href={nav.link}
-            className={`flex flex-col items-center  font-Roboto text-xl ${
+            className={`flex flex-col group items-center  font-Roboto text-xl ${
               pathname === nav.link ? 'active bg-opacity-0' : ''
             }`}
           >
-            <nav.icon className="text-4xl" />
-            <span className="hidden sm:block">{nav.name}</span>
+            <nav.icon
+              className={`text-4xl transition-all duration-300 group-hover:text-[#1a1f39] ${
+                pathname === nav.link ? 'text-[#1a1f39] ' : 'text-[#2f4158]'
+              }`}
+            />
+            <span
+              className={`hidden sm:block font-Roboto  transition-all duration-300 group-hover:text-[#1a1f39] group-hover:font-bold ${
+                pathname === nav.link
+                  ? 'text-[#1a1f39] font-bold'
+                  : 'text-[#2f4158]'
+              }`}
+            >
+              {nav.name}
+            </span>
           </Link>
         ))}
       </div>
