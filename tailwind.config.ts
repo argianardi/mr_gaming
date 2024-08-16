01c9a961168aa8ad6d393fdbaa2df0ec85b89e58
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-16 17:35:45 +0700

    Chore: Add custom screen sizes to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      exs: '360px',
+      xs: '390px',
+      xxs: '450px',
+      sx: '550px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+      '3xl': '1680px',
+      '4xl': '1920px',
+      '5xl': '2560px',
+    },
     extend: {
       fontFamily: {
         BreeSerif: ['Bree Serif'],
